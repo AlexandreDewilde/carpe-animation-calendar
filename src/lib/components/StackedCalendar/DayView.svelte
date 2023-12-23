f15ce9f07d638150fa7d7d8253619bf654708997
Alexandre Dewilde <alexandredewilde2001@gmail.com>
2023-12-23 20:07:10 +0100

    DayView: table with event

--- src/lib/components/StackedCalendar/DayView.svelte
+++ src/lib/components/StackedCalendar/DayView.svelte
@@ -1,5 +1,27 @@
 <script>
     import DayHeader from "./DayHeader.svelte";
-</script>
+    import TableHead from "./TableHead.svelte";
 
+    export let day;
+    export let events;
+    export let categories;
+
+</script>
 <DayHeader></DayHeader>
+
+<table>
+    <thead>
+        <tr>
+            <th>Today</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each events as event}
+        <tr>
+            <td>
+                {event.event.name}
+            </td>
+        </tr>
+        {/each}
+    </tbody>
+</table>
